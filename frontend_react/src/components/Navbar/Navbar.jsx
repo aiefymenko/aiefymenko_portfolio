2b92dd16f522b57e2db75e6bf7b784ecf18d427f
Artem Iefymenko <iefymenko.artem@gmail.com>
2023-11-19 15:40:24 -0500

    Create an ancor tags for fast linking

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import './Navbar.scss';
 
+import { images } from '../../constants';
+
 const Navbar = () => {
+  let elements = ['home', 'about', 'work', 'skills', 'contact'];
   return (
     <nav>
       <div>
         <img src={images.logo} alt='logo' />
       </div>
+      <ul>
+        {elements.map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
        </nav>
   )
 }
