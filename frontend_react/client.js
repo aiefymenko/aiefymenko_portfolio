0f7a9145592b6878cf5f3648774ee16c697908a5
Artem Iefymenko <iefymenko.artem@gmail.com>
2024-05-08 16:50:54 -0400

    Fix minor typos

--- frontend_react/client.js
+++ frontend_react/client.js
@@ -1,10 +1,10 @@
-import sanityClient, { SanityClient } from '@sanity/client';
+import {sanityClient} from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
-export const client = SanityClient({
+export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
-  apiVersion: '2024-05-08',
+  apiVersion: '2022-02-01',
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN
 });
