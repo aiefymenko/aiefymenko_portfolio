586c4cfbf88315d144664641c1f1fedd037a85b7
Artem Iefymenko <iefymenko.artem@gmail.com>
2024-07-01 13:26:26 -0400

    Add experience and loop through every work

--- frontend_react/src/container/Skills/Skills.jsx
+++ frontend_react/src/container/Skills/Skills.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import {motion} from 'framer-motion';
-import ReactTooltip from 'react-tooltip';
+import { Tooltip as ReactTooltip } from 'react-tooltip';
 import {AppWrap} from '../../wrapper';
 import {urlFor, client} from '../../client';
 import './Skills.scss';
@@ -18,6 +18,7 @@ const Skills = () => {
     
     client.fetch(query)
     .then((data) => {
+      console.log(data);
       setExperience(data);
     })
 
@@ -51,21 +52,43 @@ const Skills = () => {
     ))}
     </motion.div>
       <motion.div className='app__skills-exp'>
-        {experience.works.map((work) => (
-          <>
+        {experience?.map((experience) => (
           <motion.div
-                whileInView={{opacity:[0,1]}}
-                transition={{duration: 0.5}}
-                className='app__skills-exp-work'
-                data-tip
-                data-for={work.name}
-                key={work.name}
-                >
-                  <h4 className='bold-text'>{work.name}</h4>
-                  <p className='p-text'>{work.company}</p>
-          </motion.div>
-          </>
-        ))}
+          className='app__skills-exp-item'
+          key={experience.year}
+          >
+            <div className='app__skills-exp-year'>
+              <p className='bold-text'>{experience.year}</p>
+            </div>
+            <motion.div className='app__skills-exp-works'>
+              {experience.works.map((work => (
+                          <>
+                          <motion.div
+                                whileInView={{opacity:[0,1]}}
+                                transition={{duration: 0.5}}
+                                className='app__skills-exp-work'
+                                data-tip
+                                data-for={work.name}
+                                key={work.name}
+                                >
+                                  <h4 className='bold-text'>{work.name}</h4>
+                                  <p className='p-text'>{work.company}</p>
+                          </motion.div>
+                          <ReactTooltip
+                          id={work.name}
+                          effect="solid"
+                          arrowColor='#fff'
+                          className='skills-tooltip'
+                          >
+                            {work.desc}
+                          </ReactTooltip>
+                          </>
+              )))}
+              </motion.div>
+            </motion.div>
+              ))} 
+
+
       </motion.div>
    </div>
    </>
